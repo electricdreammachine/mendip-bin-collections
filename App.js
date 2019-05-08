07f4591083aacb6b35cddd94e8c0e45d805c0375
ollie smith <omsmith89@gmail.com>
2019-05-08 21:48:15 +0100

    initial get request, axios

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { get } from 'axios'
+import { startOfToday, format } from 'date-fns'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      dates: [],
+      today: '',
+    }
+  }
+  
+  componentDidMount() {
+    get('https://raw.githubusercontent.com/electricdreammachine/mendip-bin-collections-schedule/master/schedule.json')
+      .then(data => {
+        const today = startOfToday()
+        this.setState({ dates: data.data.dates, today })
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Today is {format(this.state.today, 'YYYY-MM-DD')}</Text>
+        <Text>{JSON.stringify(this.state.dates)}</Text>
+        <Text>rory thompson is a butts</Text>
       </View>
     );
   }
