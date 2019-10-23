2d8bfc007bbfeada6a892473d798f7bdfbbbb07e
Ollie <hello@olliesmith.dev>
2019-10-23 20:12:12 +0100

    starting to look at navigation

---
+++ src/components/calendar-overview/calendar-overview.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+import { Text, View } from 'react-native'
+
+const CalendarOverview = () => (
+    <View>
+        <Text>
+            Put calendars here
+        </Text>
+    </View>
+)
+
+export default CalendarOverview
